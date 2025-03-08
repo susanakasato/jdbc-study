fb3855390a27a4ae5a69f2bf46eda368bd1ec2a2
susanakasato <susana.nakasato@gmail.com>
2025-03-08 08:32:54 -0300

    Entities created

---
+++ src/model/entities/Seller.java
@@ -0,0 +1,105 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.Objects;
+
+public class Seller implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	private String name;
+	private String email;
+	private Date birthDate;
+	private Double baseSalary;
+	private Department department;
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	
+	public Seller() {}
+	
+	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.birthDate = birthDate;
+		this.baseSalary = baseSalary;
+		this.department = department;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+	
+	public void setBaseSalary(Double baseSalary) {
+		this.baseSalary = baseSalary;
+	}
+	
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public Date getBirthDate() {
+		return birthDate;
+	}
+	
+	public Double getBaseSalary() {
+		return baseSalary;
+	}
+	
+	public Department getDepartment() {
+		return department;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Seller #%d - Name: %s, Email: %s, Birth date: %s, Base salary: $%.2f, Department: %d",
+				getId(),
+				getName(),
+				getEmail(),
+				sdf.format(getBirthDate()),
+				getBaseSalary(),
+				getDepartment().getId());
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Seller other = (Seller) obj;
+		return Objects.equals(id, other.id);
+	}
+}
