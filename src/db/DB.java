cfad60ec8a48e067316ff624539f264b8fb0b59a
susanakasato <susana.nakasato@gmail.com>
2025-03-08 07:33:04 -0300

    project created and database primary setup

---
+++ src/db/DB.java
@@ -0,0 +1,69 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class DB {
+
+	private static Connection connection = null;
+	
+	public static Properties getDatabaseProperties() {
+		try (FileInputStream fs = new FileInputStream("db.properties")) {
+			Properties props = new Properties();
+			props.load(fs);
+			return props;
+		} catch (IOException e) {
+			throw new DbException(e.getMessage());
+		}
+	}
+	
+	public static Connection getDatabaseConnection() {
+		if (connection == null) {
+			Properties databaseProperties = getDatabaseProperties();
+			String databaseUrl = databaseProperties.getProperty("dburl");
+			try {
+				connection = DriverManager.getConnection(databaseUrl, databaseProperties);
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+		return connection;
+	}
+	
+	public static void closeConnection() {
+		if (connection != null) {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+	
+	public static void closeStatement(Statement statement) {
+		if (statement != null) {
+			try {
+				statement.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+	
+	public static void closeResultSet(ResultSet resultSet) {
+		if (resultSet != null) {
+			try {
+				resultSet.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+	
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module jdbcStudy {
+	requires java.sql;
+}
